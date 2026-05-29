b9c931bf86f50d394b184b24fba4ba046bf21a44
donough-chen <donough_chen@163.com>
2026-05-29 17:12:59 +0800

    feat(points):.store.index.register

--- super-tools-web/packages/h5/micro-tools/store/index.ts
+++ super-tools-web/packages/h5/micro-tools/store/index.ts
@@ -10,3 +10,10 @@ export { useMemberStore } from './member';
 export { useDeviceStore } from './device';
 export { useSendCodeStore } from './sendCode';
 export { useNotificationStore, notificationSdk } from './notification';
+
+// 积分成长体系
+export { useSignStore } from './sign';
+export { useTaskStore, selectGroupedTasks } from './task';
+export { usePointsLogStore } from './pointsLog';
+export type { DateRangeKey, PointsLogFilter } from './pointsLog';
+export { usePointsMallStore } from './pointsMall';
